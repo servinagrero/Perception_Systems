6fe1dcbdc03a7c215889c1d11f5ad90b164427bc
Sergio Vinagrero <servinagrero@gmail.com>
2019-09-16 14:41:55 +0200

    Added color histogram

---
+++ color_histogram.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace cv;
+
+const int IMG_WIDTH  = 512;
+const int IMG_HEIGHT = 512;
+const char CHANNELS = 3;
+
+int main(int argc, char **argv)
+{
+        Mat hist_img = Mat(IMG_HEIGHT, IMG_WIDTH, CV_8UC3, Scalar(0));
+        Mat img = imread("./Monkey.jpep");
+        if(!img.data)
+        {
+                std::cout << "Error loading image." <<std::endl;
+                return 1;
+        };
+
+        imshow("Original", img);
+        waitKey(0);
+        destroyWindow("Original");
+        
+        int pixel_values[CHANNELS][256] = {{0}}; // 256 values for each color
+        std::cout << "Image channels: " << img.channels() << std::endl;
+
+        const int total_pixels = img.rows * img.cols;
+        const int RATIO = 3000 / IMG_HEIGHT;
+        const int STEP = IMG_WIDTH / 256;
+        
+        for(int p = 0; p < total_pixels; ++p)
+        {
+                Vec3b pixel = img.at<Vec3b>(p);
+                pixel_values[0][(uint8_t)pixel.val[0]]++; // blue
+                pixel_values[1][(uint8_t)pixel.val[1]]++; // green
+                pixel_values[2][(uint8_t)pixel.val[2]]++; // red
+                
+        }
+        
+        for(ssize_t c = 0; c < CHANNELS; ++c)
+        {
+                for(ssize_t p = 0; p < 255;)
+                {
+                        Point start(p, IMG_HEIGHT - (pixel_values[c][p] / RATIO));
+                        p += STEP;
+                        Point end(p, IMG_HEIGHT - (pixel_values[c][p] / RATIO));
+                        switch(c)
+                        {
+                        case 0: // blue
+                                line(hist_img, start, end, Scalar(0, 0, 255));
+                                break;
+                        case 1: // green
+                                line(hist_img, start, end, Scalar(0, 255, 0));
+                                break;
+                        case 2: // red
+                                line(hist_img, start, end, Scalar(255, 0, 0));
+                                break;
+                        };
+                };
+        };
+        
+        imshow("Histogram", hist_img);
+        waitKey(0);
+        destroyWindow("Histogram");
+
+        destroyAllWindows();
+
+}
--- histogram.cpp
+++ histogram.cpp
@@ -28,7 +28,8 @@ int main(int argc, char **argv)
 
         int pixel_values[256] = {0};
 
-        std::cout << "Image channels: " << img_gray.channels() << std::endl; // Should be 1
+        // Should be 1 for a gray image
+        std::cout << "Image channels: " << img_gray.channels() << std::endl;
 
         const int total_pixels = img_gray.rows * img_gray.cols;
         const int RATIO = 3500 / IMG_HEIGHT ;
@@ -38,27 +39,17 @@ int main(int argc, char **argv)
                uint8_t pixel = img_gray.at<uint8_t>(p);
                pixel_values[pixel]++;
         }
-
-        /* for(int p = 0; p < 256; ++p) */
-        /* { */
-        /*         std::cout << "Pixel[" << p << "] = " << pixel_values[p] << std::endl; */
-        /* }; */
-
-        /* std::cout << "----------------------------------------" << std::endl; */
-
-        /* 
-         * The histogram is divided into x and y axis 
-         * X axis goes from 0 to 255 
-         * Y axis goes from 0 to (img.rows * img.cols) / RATIO
-         */
+        
+        // The histogram is divided into x and y axis
+        // X axis goes from 0 to 255
+        // Y axis goes from 0 to (img.rows * img.cols) / RATIO
         for(ssize_t v = 0; v < 256; ++v)
         {
                 Point start(v, IMG_HEIGHT);
                 Point end(v, IMG_HEIGHT - (pixel_values[v] / RATIO));
-                /* std::cout << "pixel[" << v << "] = " << start.x << " " << start.y << ", " << end.x << " " << end.y << std::endl; */
                 line(hist_img, start, end, Scalar(255, 255, 255));
         };
-
+        
         imshow("Histogram", hist_img);
         waitKey(0);
         destroyWindow("Histogram");
